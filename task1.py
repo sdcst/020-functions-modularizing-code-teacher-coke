603af085aa53725a06deb67961280dff68236493
coke老师 <justinz976512@gmail.com>
2024-05-01 13:22:52 -0700

    ez

--- task1.py
+++ task1.py
@@ -1,6 +1,18 @@
 def A():
+    print("Hello")
 
 def B():
+    print("How are you")
 
 
 def C():
+    print("Hi")
+
+
+x = input("Enter in capital letters A, B, or C: ")
+if x == "A":
+    A()
+elif x == "B":
+    B()
+else:
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,27 @@ displays instructions and how to play
 game()
 plays the games
 """
+def title():
+    print("This is a number guessing game, guess a number between 0-1000, if the entered number is over, output will be over, if number guess is under, output will be under.")
+title()
+def game():
+    import random
+    count = 0
+    x = random.randint(0, 1000)
+    y = int(input("Enter a number between 0-1000 "))
+    while y != x:
+        if y > x:
+            print("Over")
+            y = int(input("Enter a number between 0-1000 "))
+            count += 1
+        elif y < x:
+            print("Under")
+            y = int(input("Enter a number between 0-1000 "))
+            count += 1
+    if y == x:
+        print("You got the right number!!!")
+        print(f"It took you {count} tries")
+game()
+
+
+
